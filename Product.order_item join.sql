012ac8043c55a576d9e1a279b6df950ab5b97390
Uchechukwu Anya <108993926+Kingsleyyyy@users.noreply.github.com>
2023-08-19 13:36:58 +0100

    Update Product.order_item join.sql

--- Product.order_item join.sql
+++ Product.order_item join.sql
@@ -1,15 +1,1 @@
--- In this database schema, the relationship between the order_item entity
--- and products is one to many. One product can appear in multiple order_items
--- and multiple order items can reference the same product.
 
-
-select 
-oi.order_item_id,
-p.name as product_name,
-p.price as product_price,
-oi.quantity,
-oi.subtotal
-from
-order_items oi
-join 
-products p on oi.product_id = p.product_id;
